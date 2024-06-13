f98d8039a31f34469959850f2cc80de87e84d43d
Papin Nikolai <nikolai@weirdcat.ru>
2024-06-13 13:14:03 +0300

    New docs for AccountService

--- UserService/Services/Account/AccountService.cs
+++ UserService/Services/Account/AccountService.cs
@@ -1,7 +1,6 @@
 using UserService.Database.Models;
 using UserService.Models.Account.Requests;
 using UserService.Repositories;
-using Microsoft.AspNetCore.Mvc;
 using UserService.Exceptions.AccountExceptions;
 using UserService.Utils;
 
@@ -14,16 +13,6 @@ public class AccountService(IUserRepository userRepo, IUserProfileRepository use
     private readonly IRegistrationCodeRepository _regCodeRepo = regCodeRepo;
     private readonly ILogger<AccountService> _logger = logger;
 
-    /// <summary>
-    /// Finishes the registration of a new account.
-    /// </summary>
-    /// <param name="request">The request object containing the registration information.</param>
-    /// <returns>
-    /// An asynchronous task that returns an action result.
-    /// The result is either an <see cref="ActionResult{AccountTokensResponse}"/> if the registration is successful,
-    /// or an <see cref="UnauthorizedResult"/> if the registration code is invalid or expired,
-    /// or a <see cref="StatusCodeResult"/> with a status code of 409 if the username is already taken.
-    /// </returns>
     public async Task<bool> AccountFinishRegistration(AccountFinishRegistrationRequest request)
     {
         var regcode = await _regCodeRepo.GetRegistrationCodeByCode(request.RegistrationCode);
@@ -101,15 +90,6 @@ public class AccountService(IUserRepository userRepo, IUserProfileRepository use
         return true;
     }
 
-    /// <summary>
-    /// Logs in a user with the provided username and password.
-    /// </summary>
-    /// <param name="request">The login request containing the username and password.</param>
-    /// <returns>
-    /// An asynchronous task that returns an action result.
-    /// The result is either an <see cref="OkObjectResult"/> with the account tokens response if the login is successful,
-    /// or an <see cref="UnauthorizedObjectResult"/> with an error message if the username or password is invalid.
-    /// </returns>
     public async Task<bool> AccountLogin(AccountLoginRequest request)
     {
         var user = await _userRepo.GetUserByUsername(request.Username);
@@ -129,38 +109,6 @@ public class AccountService(IUserRepository userRepo, IUserProfileRepository use
         return true;
     }
 
-    /// <summary>
-    /// Refreshes the access and refresh tokens for a user.
-    /// </summary>
-    /// <param name="username">The username of the user.</param>
-    /// <param name="request">The request containing the refresh token.</param>
-    /// <returns>An asynchronous task that returns an action result with the account tokens response if the refresh is successful,
-    /// or a not found result if the user is not found.</returns>
-    [Obsolete("Feature moved to auth service")]
-    public async Task<bool> AccountRefreshToken(string username, AccountRefreshTokenRequest request)
-    {
-        var user =  await _userRepo.GetUserByUsername(username);
-
-        // May happen if the user is deleted for some reason
-        if (user == null)
-        {
-            _logger.LogWarning("Failed refresh attempt for unexistent user {Username}", username);
-            throw new TokenException("Could not find the user for the respective token in the database");
-        }
-
-        return true;
-    }
-
-    /// <summary>
-    /// Registers a new user account.
-    /// </summary>
-    /// <param name="request">The registration request containing the user's email.</param>
-    /// <returns>
-    /// An asynchronous task that returns an action result.
-    /// The result is either an <see cref="OkResult"/> if the registration is successful,
-    /// or a <see cref="StatusCodeResult"/> with a status code of 429 if the existing registration code is still valid,
-    /// or a <see cref="OkResult"/> if the registration code is expired.
-    /// </returns>
     public async Task<bool> AccountRegister(AccountRegisterRequest request)
     {
         var existingUser = await _userRepo.GetUserByEmail(request.Email);
@@ -211,17 +159,6 @@ public class AccountService(IUserRepository userRepo, IUserProfileRepository use
         return true;
     }
     
-    /// <summary>
-    /// Changes the password for a user.
-    /// </summary>
-    /// <param name="username">The username of the user.</param>
-    /// <param name="request">The request containing the old and new passwords.</param>
-    /// <returns>
-    /// An asynchronous task that returns an action result.
-    /// The result is either an <see cref="UnauthorizedResult"/> if the user does not exist,
-    /// a <see cref="BadRequestObjectResult"/> if the new password is the same as the old password or if the old password is incorrect,
-    /// or an <see cref="OkObjectResult"/> with the new access token and refresh token if the password is changed successfully.
-    /// </returns>
     public async Task<bool> AccountChangePassword(string username, AccountChangePasswordRequest request)
     {
         var user = await _userRepo.GetUserByUsername(username);
