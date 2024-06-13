9c366356422b26199e875de118b79c243761a0fc
Papin Nikolai <nikolai@weirdcat.ru>
2024-06-13 16:29:13 +0300

    Removed unused auth handler

--- DialogService/Services/MessagesService/IMessagesService.cs
+++ DialogService/Services/MessagesService/IMessagesService.cs
@@ -7,10 +7,6 @@ namespace DialogService.Services.MessagesService;
 public interface IMessagesService
 {
     public Task<Message> SendMessage(long ownerId, Sender accessor, SendMessageRequest obj);
-    [Obsolete("Unused and unmaintained")]
-    public Task<Message> EditMessage(long ownerId, Sender accessor, EditMessageRequest obj);
-    [Obsolete("Unused and unmaintained")]
-    public Task<bool> DeleteMessage(long ownerId, Sender accessor, DeleteMessageRequest obj);
     public Task<bool> RegenerateMessage(long ownerId, Sender accessor, long messageId);
     public Task<Message> GetMessage(long ownerId, Sender accessor, GetMessageRequest obj);
 }
