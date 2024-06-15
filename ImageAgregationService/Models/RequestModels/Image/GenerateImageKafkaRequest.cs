9b601f77678a8f9383b53ffb24b17b9603135689
Ferru5Manus <roma.hlinin@yandex.ru>
2024-06-15 22:47:04 +0300

    Refactoring image work

--- ImageAgregationService/Models/RequestModels/Image/GenerateImageKafkaRequest.cs
+++ ImageAgregationService/Models/RequestModels/Image/GenerateImageKafkaRequest.cs
@@ -4,5 +4,12 @@ namespace ImageAgregationService.Models.RequestModels
     {
         public string TemplateName {get; set;} = null!;
         public string Text {get; set;} = null!;
+        public string ImageText{get; set;} = null!;
+        public List<string> AllowedColors {get; set;} = null!;
+        public Resolution Resolution {get; set;} = null!;
+        public ResolutionPosition Position {get; set;} = null!;
+        public string FontName {get; set;} = null!;
+        public string Background {get; set;} = null!;
+        public bool CheckColours {get; set;} = true;
     }
 }
