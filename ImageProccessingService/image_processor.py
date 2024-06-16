ceb4581b657d8af72947146cc480f000026d8128
Ferru5Manus <roma.hlinin@yandex.ru>
2024-06-16 22:27:01 +0300

    FF

--- ImageProccessingService/image_processor.py
+++ ImageProccessingService/image_processor.py
@@ -219,7 +219,7 @@ class GExchange(pb2_grpc.ImageProcessorServicer):
          image = process(request.byte_image, request.allowed_colors_str, request.background, request.width, request.height, request.resolution_pos, request.text, request.font, request.should_check_colors)
          return pb2.ImageResponse(error="", image_byte_array=image)
        except Exception as e:
-         return pb2.ImageResponse(error=str(e), image_byte_array=[])
+         return pb2.ImageResponse(error=str(e), image_byte_array=b'some data')
        
 def serve():
    server = grpc.server(futures.ThreadPoolExecutor(max_workers=100))
